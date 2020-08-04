462b1ee08812fe1107ba663541c96421ac2c3ce2
Ivan Rusanov <ivan.b.rusanov@gmail.com>
2020-08-05 02:25:09 +0300

    Improve Scan Result dialog layout and make it resizable

--- ResultForm.Designer.cs
+++ ResultForm.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace ZenStatesDebugTool
+namespace ZenStatesDebugTool
 {
     partial class ResultForm
     {
@@ -30,14 +30,18 @@
         {
             this.textBoxFormResult = new System.Windows.Forms.TextBox();
             this.buttonSaveResult = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
             this.buttonSaveAs = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // textBoxFormResult
             // 
             this.textBoxFormResult.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.tableLayoutPanel1.SetColumnSpan(this.textBoxFormResult, 2);
+            this.textBoxFormResult.Dock = System.Windows.Forms.DockStyle.Fill;
             this.textBoxFormResult.Font = new System.Drawing.Font("Consolas", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBoxFormResult.Location = new System.Drawing.Point(13, 13);
+            this.textBoxFormResult.Location = new System.Drawing.Point(3, 3);
             this.textBoxFormResult.Multiline = true;
             this.textBoxFormResult.Name = "textBoxFormResult";
             this.textBoxFormResult.ReadOnly = true;
@@ -48,7 +52,7 @@
             // 
             // buttonSaveResult
             // 
-            this.buttonSaveResult.Location = new System.Drawing.Point(13, 265);
+            this.buttonSaveResult.Location = new System.Drawing.Point(3, 248);
             this.buttonSaveResult.Name = "buttonSaveResult";
             this.buttonSaveResult.Size = new System.Drawing.Size(75, 23);
             this.buttonSaveResult.TabIndex = 1;
@@ -56,33 +60,47 @@
             this.buttonSaveResult.UseVisualStyleBackColor = true;
             this.buttonSaveResult.Click += new System.EventHandler(this.buttonSaveResult_Click);
             // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.buttonSaveAs, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.textBoxFormResult, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.buttonSaveResult, 0, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(247, 274);
+            this.tableLayoutPanel1.TabIndex = 3;
+            // 
             // buttonSaveAs
             // 
-            this.buttonSaveAs.Location = new System.Drawing.Point(95, 264);
+            this.buttonSaveAs.Location = new System.Drawing.Point(84, 248);
             this.buttonSaveAs.Name = "buttonSaveAs";
             this.buttonSaveAs.Size = new System.Drawing.Size(75, 23);
-            this.buttonSaveAs.TabIndex = 2;
+            this.buttonSaveAs.TabIndex = 4;
             this.buttonSaveAs.Text = "Save As...";
             this.buttonSaveAs.UseVisualStyleBackColor = true;
-            this.buttonSaveAs.Click += new System.EventHandler(this.buttonSaveAs_Click);
             // 
             // ResultForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(266, 295);
-            this.Controls.Add(this.buttonSaveAs);
-            this.Controls.Add(this.buttonSaveResult);
-            this.Controls.Add(this.textBoxFormResult);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
-            this.MaximizeBox = false;
+            this.ClientSize = new System.Drawing.Size(247, 274);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
             this.Name = "ResultForm";
-            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
             this.TopMost = true;
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -90,6 +108,7 @@
 
         public System.Windows.Forms.TextBox textBoxFormResult;
         private System.Windows.Forms.Button buttonSaveResult;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
         private System.Windows.Forms.Button buttonSaveAs;
     }
 }
